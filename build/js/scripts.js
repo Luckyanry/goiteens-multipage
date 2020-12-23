52188db10def04a9f7e0e32d83a3e47cac0ee48d
Olexiy-repin <repinlyoha@gmail.com>
2020-12-23 18:03:30 +0200

    Added some section

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -10,3 +10,40 @@
     mobileMenuRef.classList.toggle("is-open");
   });
 })();
+
+(function () {
+  $('.courses__img-link').magnificPopup({
+    type: 'image'
+  });
+})();
+
+(function () {
+  $('.testimonials__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    mobileFirst: true,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        asNavFor: '.testimonials__nav-slider'
+      }
+    }]
+  });
+  $('.testimonials__nav-slider').slick({
+    infinite: true,
+    arrows: false,
+    mobileFirst: true,
+    focusOnSelect: true,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        asNavFor: '.testimonials__slider'
+      }
+    }]
+  });
+})();
--- src/js/menu.js
+++ src/js/menu.js
@@ -11,4 +11,44 @@
   
       mobileMenuRef.classList.toggle("is-open");
     });
-  })();
+})();
+
+(() => {
+  $('.courses__img-link').magnificPopup({type:'image'});
+})();
+
+(() => {
+  $('.testimonials__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    mobileFirst: true,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          asNavFor: '.testimonials__nav-slider',
+        }
+      }
+    ]
+  });
+
+  $('.testimonials__nav-slider').slick({
+    infinite: true,
+    arrows: false,
+    mobileFirst: true,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 1,
+          asNavFor: '.testimonials__slider'
+        }
+      }
+    ]
+  });
+})();
