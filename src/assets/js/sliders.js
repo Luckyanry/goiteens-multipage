236360659d9eb2eac07b3e167ccaa2bfa1045456
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2021-07-07 19:52:20 +0300

    rewrite mixins / deleted comments / replace fonts mixin to all pages

--- src/assets/js/sliders.js
+++ src/assets/js/sliders.js
@@ -1,11 +1,10 @@
 import $ from 'jquery';
-import { slick } from 'slick-carousel';
+import slick from 'slick-carousel';
 
 $('.awards-slider-js').slick({
   mobileFirst: true,
   arrows: false,
   dots: true,
-  // adaptiveHeight: true,
   slidesToShow: 1,
   slidesToScroll: 1,
   speed: 400,
@@ -101,78 +100,82 @@ $('.projects-slider-js').slick({
     },
   ],
 });
+/*****************
+ * НА ЭТИХ ДВУХ СЛАЙДЕРАХ ПАДАЕТ СКРИПТ И ПОЛУЧАЕМ ОШИБКУ
+ * СКОРЕЕ ВСЕГО ЭТО СВЯЗАНО С ТЕМ, ЧТО КНОПКИ prev-but-js/next-but-js
+ * ЛИБО ОТСУТСТВУЮТ ЛИБО ОТПАЛИ В РАЗМЕТКЕ/СТИЛЯХ
+ ******************/
 
-$('.review-slider-js').slick({
-  mobileFirst: true,
-  lazyLoad: 'ondemand',
-  dots: true,
-  arrows: false,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  speed: 400,
-  infinite: true,
-  cssEase: 'linear',
-  centerMode: true,
-  centerPadding: 0,
-  responsive: [
-    {
-      breakpoint: 1599,
-      settings: {
-        arrows: true,
-        focusOnSelect: true,
-        centerPadding: '22%',
-        appendArrows: '.review-button',
-        prevArrow: $('.prev-but-js'),
-        nextArrow: $('.next-but-js'),
-      },
-    },
-    {
-      breakpoint: 1279,
-      settings: {
-        arrows: true,
-        focusOnSelect: true,
-        centerPadding: '20%',
-        appendArrows: '.review-button',
-        prevArrow: $('.prev-but-js'),
-        nextArrow: $('.next-but-js'),
-      },
-    },
-    {
-      breakpoint: 767,
-      settings: {
-        arrows: true,
-        // centerPadding: '115px 0 0',
-        appendArrows: '.review-button',
-        prevArrow: $('.prev-but-js'),
-        nextArrow: $('.next-but-js'),
-      },
-    },
-  ],
-});
+// $('.review-slider-js').slick({
+//   mobileFirst: true,
+//   lazyLoad: 'ondemand',
+//   dots: true,
+//   arrows: false,
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   speed: 400,
+//   infinite: true,
+//   cssEase: 'linear',
+//   centerMode: true,
+//   centerPadding: 0,
+//   responsive: [
+//     {
+//       breakpoint: 1599,
+//       settings: {
+//         arrows: true,
+//         focusOnSelect: true,
+//         centerPadding: '22%',
+//         appendArrows: '.review-button',
+//         prevArrow: $('.prev-but-js'),
+//         nextArrow: $('.next-but-js'),
+//       },
+//     },
+//     {
+//       breakpoint: 1279,
+//       settings: {
+//         arrows: true,
+//         focusOnSelect: true,
+//         centerPadding: '20%',
+//         appendArrows: '.review-button',
+//         prevArrow: $('.prev-but-js'),
+//         nextArrow: $('.next-but-js'),
+//       },
+//     },
+//     {
+//       breakpoint: 767,
+//       settings: {
+//         arrows: true,
+//         appendArrows: '.review-button',
+//         prevArrow: $('.prev-but-js'),
+//         nextArrow: $('.next-but-js'),
+//       },
+//     },
+//   ],
+// });
 
-$('.projects-slider-js').slick({
-  mobileFirst: true,
-  dots: true,
-  arrows: false,
-  slidesToScroll: 1,
-  slidesToShow: 1,
-  speed: 400,
-  infinite: true,
-  cssEase: 'linear',
-  responsive: [
-    {
-      breakpoint: 767,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-      },
-    },
-    {
-      breakpoint: 1279,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-      },
-    },
-  ],
-});
+// $('.projects-slider-js').slick({
+//   mobileFirst: true,
+//   dots: true,
+//   arrows: false,
+//   slidesToScroll: 1,
+//   slidesToShow: 1,
+//   speed: 400,
+//   infinite: true,
+//   cssEase: 'linear',
+//   responsive: [
+//     {
+//       breakpoint: 767,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 1,
+//       },
+//     },
+//     {
+//       breakpoint: 1279,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 3,
+//       },
+//     },
+//   ],
+// });
