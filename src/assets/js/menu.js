12e1dea3504f9f5a4710cad342ac9bc17cac4195
kharchenkok <k.v.kharchenko@ukr.net>
2021-07-13 12:10:46 +0300

    navlink current added

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
+    const navLinks = document.querySelectorAll(".navigation__list-link")
 
     menuBtnRef.addEventListener("click", () => {
       const expanded =
@@ -12,192 +13,25 @@ import $ from 'jquery';
       menuBtnRef.setAttribute("aria-expanded", !expanded);
 
       mobileMenuRef.classList.toggle("is-open");
+
     });
+    
+  
+   
+    function setActiveNavLink(items) {
+    
+      for (let index = 0; index < items.length; index++) {
+        const link = items[index];
+        if (link.href === window.location.href) {
+          link.classList.add('navigation__list-link_current');
+        }
+      }
+    }
+
+    setActiveNavLink(navLinks)
+
+
 })();
 
-(() => {
-  if(document.querySelector('.courses__img-link')) {
-    $('.courses__img-link').magnificPopup({type:'image'});
-  }
-})();
 
-(() => {
-  if(document.querySelector('.testimonials__slider')) {
-    $('.testimonials__slider').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      mobileFirst: true,
-      prevArrow: '<button type="button" class="slick-prev"></button>',
-      nextArrow: '<button type="button" class="slick-next"></button>',
-      fade: true,
-      cssEase: 'linear',
-      speed: 1000,
-      responsive: [
-        {
-          breakpoint: 767,
-          settings: {
-            asNavFor: '.testimonials__nav-slider',
-          }
-        }
-      ]
-    });
 
-    $('.testimonials__nav-slider').slick({
-      infinite: true,
-      arrows: false,
-      mobileFirst: true,
-      focusOnSelect: true,
-      responsive: [
-        {
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 6,
-            slidesToScroll: 1,
-            asNavFor: '.testimonials__slider'
-          }
-        }
-      ]
-    });
-  }
-})();
-
-(() => {
-  if(document.querySelector('.teacher__slider')){
-    $('.teacher__slider').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      mobileFirst: true,
-      centerMode: true,
-      centerPadding: "90px",
-      asNavFor: '.teacher__slider-content',
-      prevArrow: '<button type="button" class="slick-prev"></button>',
-      nextArrow: '<button type="button" class="slick-next"></button>',
-    });
-
-    $('.teacher__slider-content').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      mobileFirst: true,
-      swipe: false,
-    });
-
-    $('.teacher__slider_desktop').slick({
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      mobileFirst: true,
-      prevArrow: '<button type="button" class="slick-prev"></button>',
-      nextArrow: '<button type="button" class="slick-next"></button>',
-      responsive: [
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 4,
-          }
-        }
-      ]
-    });
-
-    $('.speakers__slider').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      mobileFirst: true,
-      centerMode: true,
-      centerPadding: "90px",
-      asNavFor: '.speakers__slider-content',
-      prevArrow: '<button type="button" class="slick-prev"></button>',
-      nextArrow: '<button type="button" class="slick-next"></button>',
-    });
-
-    $('.speakers__slider-content').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      mobileFirst: true,
-      swipe: false,
-    });
-
-    $('.speakers__slider_desktop').slick({
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      mobileFirst: true,
-      prevArrow: '<button type="button" class="slick-prev"></button>',
-      nextArrow: '<button type="button" class="slick-next"></button>',
-      responsive: [
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 4,
-          }
-        }
-      ]
-    });
-  }
-})();
-
-(() => {
-  if(document.querySelector('.testimonials__students-slider')){
-    $('.testimonials__students-slider').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      mobileFirst: true,
-      prevArrow: '<button type="button" class="slick-prev"></button>',
-      nextArrow: '<button type="button" class="slick-next"></button>',
-      fade: true,
-      cssEase: 'linear',
-      speed: 1000,
-      responsive: [
-        {
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 2,
-            fade: false,
-          },
-          breakpoint: 1199,
-          settings: {
-            slidesToShow: 4,
-            fade: false,
-          }
-        }
-      ]
-    });
-  }
-})();
-
-(() => {
-  if(document.querySelector('.testimonials__parents-slider')){
-    $('.testimonials__parents-slider').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      mobileFirst: true,
-      prevArrow: '<button type="button" class="slick-prev"></button>',
-      nextArrow: '<button type="button" class="slick-next"></button>',
-      fade: true,
-      cssEase: 'linear',
-      speed: 1000,
-      responsive: [
-        {
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 2,
-            fade: false,
-          },
-          breakpoint: 1199,
-          settings: {
-            slidesToShow: 4,
-            fade: false,
-          }
-        }
-      ]
-    });
-  }
-})();
