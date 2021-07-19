a3dcf6e3b890868768fc9e7fc3f64632ea1e8ffa
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2021-07-20 00:09:35 +0300

    fix menu animation / modals side effects

--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -1,68 +1,26 @@
-(() => {
-    const refs = {
-      openModalBtn: [...document.querySelectorAll('[data-modal-open]')],
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
-    };
+const refs1 = {
+  openTermsBtn: [...document.querySelectorAll('[data-terms]')],
+  closeTermsBtn: [...document.querySelectorAll('[data-terms-close]')],
+  termsModal: [...document.querySelectorAll('[data-termsmodal]')],
+};
 
-    refs.openModalBtn.forEach(e => {
-      e.addEventListener('click', toggleModal);
-    });
-    refs.closeModalBtn.addEventListener('click', toggleModal);
+refs1.openTermsBtn.forEach(e => {
+  e.addEventListener('click', () => {
+    e.preventDefault;
+    toggleModal1(e.dataset.terms);
+  });
+});
 
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
+function toggleModal1(currentModal) {
+  refs1.termsModal.forEach(e => {
+    if (e.dataset.termsmodal === `${currentModal}`) {
+      e.classList.toggle('is-hidden');
     }
-
-    // Закрытие модального окна по клику на esc
-    window.addEventListener('keydown', (e) => {
-      if (!refs.modal.classList.contains('is-hidden')) {
-        if(e.keyCode === 27) {
-          refs.modal.classList.toggle('is-hidden');
-        }
-      }
-      return;
-    });
-
-    // Закрытие модального окна по клику на backdrop
-    window.addEventListener('click', (e) => {
-      if (!refs.modal.classList.contains('is-hidden')) {
-        if(e.target.classList.contains('backdrop')) {
-          refs.modal.classList.toggle('is-hidden');
-        }
-      }
-      return;
-    });
-
-  })();
-
-(() => {
-  const refs = {
-    openTermsBtn: [...document.querySelectorAll('[data-terms]')],
-    closeTermsBtn: [...document.querySelectorAll('[data-terms-close]')],
-    termsModal: [...document.querySelectorAll('[data-termsmodal]')],
-  };
-
-  refs.openTermsBtn.forEach(e => {
-    e.addEventListener("click", () => {
-      e.preventDefault;
-      toggleModal(e.dataset.terms);
-    });
   });
+}
 
-  function toggleModal(currentModal) {
-    refs.termsModal.forEach(e => {
-      if(e.dataset.termsmodal === `${currentModal}`) {
-        e.classList.toggle('is-terms-hidden');
-      }
-    });
-  }
-
-  refs.closeTermsBtn.forEach(e => {
-    e.addEventListener('click', () => {
-      e.closest('.backdrop').classList.toggle('is-terms-hidden');
-    });
+refs1.closeTermsBtn.forEach(e => {
+  e.addEventListener('click', () => {
+    e.closest('.backdrop').classList.toggle('is-hidden');
   });
-
- 
-})();
+});
